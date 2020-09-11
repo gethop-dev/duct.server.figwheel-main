2d689a6ae09f30f0e80241f0c0d2f16d85575640
Damian Hryniewicz <damian@magnet.coop>
2020-09-11 11:03:34 +0200

    Reinitialize the ig key only when necessary

--- src/duct/server/figwheel_main.clj
+++ src/duct/server/figwheel_main.clj
@@ -4,13 +4,15 @@
 
 (defmethod ig/init-key :duct.server/figwheel-main [_ opts]
   (figwheel/start opts)
-  opts)
+  {:server (:id opts)})
 
-(defmethod ig/halt-key! :duct.server/figwheel-main [_ {:keys [id]}]
-  (figwheel/stop id))
+(defmethod ig/halt-key! :duct.server/figwheel-main [_ {:keys [server]}]
+  (figwheel/stop server))
 
 (defmethod ig/suspend-key! :duct.server/figwheel-main [_ _])
 
 (defmethod ig/resume-key :duct.server/figwheel-main [key opts old-opts old-impl]
-  (do (ig/halt-key! key old-impl)
-      (ig/init-key key opts)))
+  (if (and (:server old-impl) (= opts old-opts))
+    old-impl
+    (do (ig/halt-key! key old-impl)
+        (ig/init-key key opts))))
